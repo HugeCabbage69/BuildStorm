cd24b6959d0f4b3ea6d14979f1dd37f22e25c726
HugeCabbage69 <139630387+HugeCabbage69@users.noreply.github.com>
2026-03-16 02:14:14 +0530

    Index and images

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* register button interaction */
-
 const registerBtn = document.getElementById("registerBtn")
 
 registerBtn.addEventListener("click", () => {
@@ -7,10 +5,6 @@ registerBtn.addEventListener("click", () => {
 registerBtn.innerText = "Registration Opening Soon 🚀"
 
 })
-
-
-
-/* countdown timer */
 
 const eventDate = new Date("2026-08-01")
 
@@ -21,9 +15,7 @@ const now = new Date()
 const diff = eventDate - now
 
 const days = Math.floor(diff / (1000*60*60*24))
-
 const hours = Math.floor(diff / (1000*60*60) % 24)
-
 const minutes = Math.floor(diff / (1000*60) % 60)
 
 document.getElementById("days").innerText = days
